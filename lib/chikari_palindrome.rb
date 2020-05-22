1fa4d3e4602e8d10539a92b30d570d0284939131
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-05-22 19:49:08 +0200

    Initialize repo

---
+++ lib/chikari_palindrome.rb
@@ -0,0 +1,6 @@
+require "chikari_palindrome/version"
+
+module ChikariPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "chikari_palindrome"
+
+require "minitest/autorun"
